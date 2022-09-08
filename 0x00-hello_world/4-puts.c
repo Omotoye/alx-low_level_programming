354d00f142eeb368a7243ddc50c309a6e948700a
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-08 04:13:50 -0700

    :art: improve structure of code - betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /**
- * Author: Omotoye Shamsudeen Adekoya 
+ * Author: Omotoye Shamsudeen Adekoya
  * Purpose: use puts() to print to standard output
  * main - Entry point
  *
  * Return: Always 0 (Sucess)
  */
 
-int main(void) 
+int main(void)
 {
 	/* String Initialization */
 	char message[] = "\"Programming is like building a multilingual puzzle";
-	
 	/* Writing the string to stdout */
-	puts(message); 
-
-	return 0; 
+	puts(message);
+	return (0);
 }
