8830a0e6ffd15b9578764f56214540d9c8b9a813
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-08 04:52:42 -0700

    :construction: C program that prints sizeof types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a program that prints the size of various types on the
+ *     computer it is compiled and run on
+ * Return: Always 0 (Success)
+ * Author: Omotoye Shamsudeen Adekoya
+ */
+
+int main(void)
+{
+	char char_type;
+	int int_type;
+	long int long_int_type;
+	long long int long_long_int_type;
+	float float_type;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(char_type));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int_type));
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(long_int_type));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_int_type));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float_type));
+	return (0);
+}
