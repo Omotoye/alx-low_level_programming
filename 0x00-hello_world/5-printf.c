3eca2c4f97846c3b8d93772f8c44b0dbdb9cca1d
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-08 04:33:54 -0700

    :construction: C code that uses printf to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a program that prints a string with printf
+ * Return: Always 0 (Sucess)
+ * Author: Omotoye Shamsudeen Adekoya
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
