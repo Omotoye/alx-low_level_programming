796956af6f56cbb2540bfd420de947ccef0a1fad
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-08 09:13:35 -0700

    :construction: C program to print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a program that prints to standard error
+ * Return: Always 0 (Success)
+ * Author: Omotoye Shamsudeen Adekoya
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
