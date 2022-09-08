9845eea15d1e2b6f650c48508c9040043049168a
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-08 03:53:20 -0700

    :construction: C code to print to stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Author: Omotoye Shamsudeen Adekoya 
+ * Purpose: use puts() to print to standard output
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void) 
+{
+	// String Initialization
+	char message[] = "Programming is like building a multilingual puzzle";
+	puts(message); // writing the string to stdout 
+
+	return 0; 
+}
