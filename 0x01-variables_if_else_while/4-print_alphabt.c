228aa2a21ba817e36b00242f1abf0f1a04ccc72e
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-10 04:05:23 +0100

    :hammer: program to print alphabets except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: a program to print all the alphabet
+ *     in lowercase except 'q' and 'e'
+ * Return: Always 0 (Success)
+ * Author: Omotoye Shamsudeen Adekoya
+ */
+
+int main(void)
+{
+	char alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+	if ((alphabet[i] != 'q') && (alphabet[i] != 'e'))
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
