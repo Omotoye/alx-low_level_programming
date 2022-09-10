018f5b040b6c9451612863bea3804daf848a9c11
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-10 05:39:18 +0100

    :ambulance: major changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,13 +16,13 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		putchar((i + '0'));
+		if (i != 9)
 		{
-			putchar((i + '0'));
-			putchar((j + '0'));
 			putchar(',');
-			putchar(' ');
 		}
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
