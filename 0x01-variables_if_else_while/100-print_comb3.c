6851bb135adb62bb73355f7b8935b09ae0c4f62e
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-10 06:01:44 +0100

    :bug: fixing issues in code

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: a program to print all possible combination of
+ *     single digit numbers
+ * Return: Always 0 (Success)
+ * Author: Omotoye Shamsudeen Adekoya
+ */
+
+int main(void)
+{
+    int i;
+    int j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            if (i != j)
+            {
+                putchar((i + '0'));
+                putchar((j + '0'));
+                if (i != 9)
+                {
+                    putchar(',');
+                }
+                putchar(' ');
+            }
+                }
+    }
+    return (0);
+}
