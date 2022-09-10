0b4a3bf7da7015616568abbea47c3b7648ce6ffc
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-10 02:29:31 +0100

    :adhesive_bandage: minor changes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,25 +13,26 @@
 
 int main(void)
 {
-	int n;
-    char last_digit;
+int n;
+short int last_digit;
+char tmp_msg[20] = "Last digit of ";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-    last_digit = n % 10;
+last_digit = abs(n % 10);
 
 	if (last_digit > 5)
 	{
-		printf("Last digit of %d is %c and is greater than 5\n", n, last_digit);
+		printf("%s%d is %d and is greater than 5\n", tmp_msg, n, last_digit);
 	}
 	else if ((last_digit < 6) && last_digit != 0)
 	{
-		printf("Last digit of %d is %c and is less than 6 and not 0\n", n, last_digit);
+		printf("%s%d is %d and is less than 6 and not 0\n", tmp_msg, n, last_digit);
 	}
 	else
 	{
-		printf("Last digit of %d is %c and is 0\n", n, last_digit);
+		printf("%s%d is %d and is 0\n", tmp_msg, n, last_digit);
 	}
 
 	return (0);
