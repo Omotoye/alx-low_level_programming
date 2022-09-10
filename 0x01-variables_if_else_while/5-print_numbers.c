3830f4550b28ad3fcf5122e4ef92d768962c34be
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-10 04:16:22 +0100

    :hammer: program to print single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: a program to print all single digit
+ *     numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ * Author: Omotoye Shamsudeen Adekoya
+ */
+
+int main(void)
+{
+	char single_digits[] = "0123456789";
+	int i;
+
+	printf("%s\n", single_digits);
+	return (0);
+}
