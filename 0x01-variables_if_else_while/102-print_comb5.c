2ccdb3926858d5da61ce4de3e3a4cfb4fbde954f
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-11 23:03:40 +0100

    minor changes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -27,11 +27,11 @@ int main(void)
 				{
 					if (!((a == c && b == d) || (b == c && a == d)))
 					{
-						putchar((c + '0'));
-						putchar((d + '0'));
-						putchar(' ');
 						putchar((a + '0'));
 						putchar((b + '0'));
+						putchar(' ');
+						putchar((c + '0'));
+						putchar((d + '0'));
 					if (a != 9 || b != 9 || c != 9 || d != 8)
 					{
 						putchar(',');
