d0ad3a301da40d93d8c61d5a0d8da9d081987573
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-13 23:21:25 +0100

    :art: improved the commenting and structure of the code

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: a program to print _putchar followed by
- *     a newline
- * Return: Always 0 (Success)
- * Author: Omotoye Shamsudeen Adekoya
+ * _putchar - puts character in stdout
+ * @c: (char) the character to be printed
+ *
+ * @brief A function to put a character into the standard
+ *     output file.
+ * Return: (void)
+ * @file _putchar.c
+ * @author Omotoye Shamsudeen Adekoya
  */
-
 void _putchar(char c)
 {
-    putchar(c);
+	putchar(c);
 }
