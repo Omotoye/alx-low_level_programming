3691341b9c83d53153138da303aeb4445706a7c4
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-13 21:50:58 +0100

    :hammer: added function to print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - function
+ * @brief A function to print every alphabet in lowercase
+ *     10 times.
+ * @file 2-print_alphabet_x10.c
+ * @param void
+ * @return void
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void print_alphabet_x10(void)
+{
+    char alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
+    int i;
+    int j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 26; j++)
+        {
+            putchar(alphabet[j]);
+        }
+        putchar('\n');
+    }
+}
