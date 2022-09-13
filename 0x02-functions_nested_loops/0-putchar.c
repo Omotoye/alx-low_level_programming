2bd063e78b755e4684f80f1f15bd4bb867e6155d
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-13 20:27:24 +0100

    :hammer: added program that prints _putchar and newline

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: a program to print _putchar followed by
+ *     a newline
+ * Return: Always 0 (Success)
+ * Author: Omotoye Shamsudeen Adekoya
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
