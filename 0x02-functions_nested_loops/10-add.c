12c2a60f109c3054e343a8619a61206a859a3552
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-14 02:55:24 +0100

    :hammer: added a function to add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ * @num1: (int) integer 1; to be added to integer 2
+ * @num2: (int) integer 2; to be added to integer 1
+ * @brief A function that takes two integers, add
+ *     them together and return the sum
+ * Return: (int) the sum of the two integer arguments
+ * @file 10-add.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -113,4 +113,16 @@ void jack_bauer(void);
  * @author Omotoye Shamsudeen Adekoya
  */
 void times_table(void);
+
+/**
+ * add - adds two integers
+ * @num1: (int) integer 1; to be added to integer 2
+ * @num2: (int) integer 2; to be added to integer 1
+ * @brief A function that takes two integers, add
+ *     them together and return the sum
+ * Return: (int) the sum of the two integer arguments
+ * @file 10-add.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+int add(int num1, int num2);
 #endif /* MAIN_H */
