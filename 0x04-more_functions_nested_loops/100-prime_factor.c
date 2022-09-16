911633f3555a483b789b24d21e98f76e79871cba
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-16 05:58:04 +0100

    testing

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - print square
+ * @size: (int) the size of the square
+ *
+ * @brief A function that prints a square on the
+ *     terminal based on the size given.
+ * Return: (void)
+ * @file 8-print_square.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void prime_factor(int n){
+	
+}
+
+int main(){
+    print_number(5);
+}
