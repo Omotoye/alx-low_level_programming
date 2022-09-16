639e57d67c7d09bf823a29788689c335a8fb6f71
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-16 03:06:46 +0100

    :hammer: added function that multiply two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply two int
+ * @a: (int) the first integer to be multiplied
+ * @b: (int) the second integer to be multiplied
+ *
+ * @brief A function that takes in two integers,
+ *     multiplys them together and return the result.
+ * Return: (int) the product of the two integer arguments
+ * @file 2-mul.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -39,4 +39,17 @@ int _isupper(int c);
  */
 int _isdigit(int c);
 
+/**
+ * mul - multiply two int
+ * @a: (int) the first integer to be multiplied
+ * @b: (int) the second integer to be multiplied
+ *
+ * @brief A function that takes in two integers,
+ *     multiplys them together and return the result.
+ * Return: (int) the product of the two integer arguments
+ * @file 2-mul.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+int mul(int a, int b);
+
 #endif /* MAIN_H */
