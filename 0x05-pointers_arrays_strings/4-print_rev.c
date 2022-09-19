92e0648a269a83452eba5f7e43789658bfec27ba
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-19 21:00:54 +0100

    :hammer: added function to print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: (char*) pointer to the string to be printed in reverse
+ *
+ * @brief A function that takes in a string and prints it
+ *     to stdout in reverse
+ * Return: (void)
+ * @file 4-print_rev.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void print_rev(char *s)
+{
+	int n = strlen(s);
+
+	while (n >= 0)
+	{
+		putchar(*(s + n));
+		n--;
+	}
+	putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -50,4 +50,16 @@ int _strlen(char *s);
  */
 void _puts(char *str);
 
+/**
+ * print_rev - prints a string in reverse
+ * @s: (char*) pointer to the string to be printed in reverse
+ *
+ * @brief A function that takes in a string and prints it
+ *     to stdout in reverse
+ * Return: (void)
+ * @file 4-print_rev.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void print_rev(char *s);
+
 #endif /* MAIN_H */
