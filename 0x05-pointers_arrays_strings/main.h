35bf88c1f669fbccf00437fcf8bb2199f816acb0
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-19 19:43:17 +0100

    :hammer: added program to update a pointer value

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * reset_to_98 - update variable value to 98
+ * @n: (int*) pointer to the integer value to be modified
+ *
+ * @brief A function that takes in a pointer to an integer value
+ *     and updates it to 98.
+ * Return: (void)
+ * @file 0-reset_to_98.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
