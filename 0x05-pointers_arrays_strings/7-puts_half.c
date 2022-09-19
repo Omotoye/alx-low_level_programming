efc3d9de3936ad53d5fc6ccd8ec7ee6b59bf180e
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-19 22:57:18 +0100

    :hammer: added function that prints the second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - print the second half of a string
+ * @str: (char*) pointer to the string to be printed
+ *
+ * @brief A function that prints the second half of a string.
+ * Return: (void)
+ * @file 7-puts_half.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void puts_half(char *str)
+{
+	int n = strlen(str) / 2;
+
+	str = str + n;
+
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -85,4 +85,15 @@ void rev_string(char *s);
  */
 void puts2(char *str);
 
+/**
+ * puts_half - print the second half of a string
+ * @str: (char*) pointer to the string to be printed
+ *
+ * @brief A function that prints the second half of a string.
+ * Return: (void)
+ * @file 7-puts_half.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void puts_half(char *str);
+
 #endif /* MAIN_H */
