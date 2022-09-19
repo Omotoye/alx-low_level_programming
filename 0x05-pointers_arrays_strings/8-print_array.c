2beff891e4d5f8b1121f01da92879c40f9361f09
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-19 23:32:31 +0100

    :ambulance: more fixes

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,15 +15,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n < 0){
-		n = n * (-1);
-	}
-
 	for (i = 0; i < n; i++)
 	{
 		if (i != n - 1)
 			printf("%d, ", *(a + i));
 		else
-			printf("%d\n", *(a + i));
+			printf("%d", *(a + i));
 	}
+	putchar('\n');
 }
