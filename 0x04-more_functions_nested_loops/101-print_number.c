5f0194cc38165e4dd2a1a3adc92f861fe0386d1d
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-19 19:33:21 +0100

    :memo: updated the readme

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,10 +11,46 @@
  * @file 8-print_square.c
  * @author Omotoye Shamsudeen Adekoya
  */
-void print_number(int n){
-	
+
+int get_digit_count(int num);
+void print_number(int n)
+{
+    int multiplier = 10^(get_digit_count(n));
+    printf("%d\n", get_digit_count(n));
+    printf("%d\n", multiplier);
+    int digit;
+    while (multiplier >= 10)
+    {
+        digit = (n / multiplier) % 10;
+        // putchar(digit);
+        multiplier /= 10;
+    }
 }
 
-int main(){
-    print_number(5);
+int get_digit_count(int num)
+{
+    int digit_count = 1;
+    if (num < 0)
+        num = num * (-1);
+    while (num >= 10)
+    {
+        num = num / 10;
+        digit_count++;
+    }
+    return digit_count;
+}
+
+int main()
+{
+    print_number(98);
+    putchar('\n');
+    print_number(402);
+    putchar('\n');
+    print_number(1024);
+    putchar('\n');
+    print_number(0);
+    putchar('\n');
+    print_number(-98);
+    putchar('\n');
+    return (0);
 }
