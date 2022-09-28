fc206f02cd497c97701e3090d0a0dcae90325dd6
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-09-28 12:50:54 +0100

    :hammer: added a function to print a string of character recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ * @s: (char*) pointer to the string to be printed
+ *
+ * @brief A function that prints a string and then a newline
+ *     with a recursive function
+ * Return: (void)
+ * @file 0_puts_recursion.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void _puts_recursion(char *s)
+{
+	/* base case */
+	if (*s == '\0')
+	{
+		putchar('\n');
+	}
+	else
+	{
+		putchar(*s);
+		_puts_recursion(++s);
+	}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character in stdout
+ * @c: (char) the character to be printed
+ *
+ * @brief A function to put a character into the standard
+ *     output file.
+ * Return: (void)
+ * @file _putchar.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void _putchar(char c);
+
+/**
+ * _puts_recursion - prints a string
+ * @s: (char*) pointer to the string to be printed
+ *
+ * @brief A function that prints a string and then a newline
+ *     with a recursive function
+ * Return: (void)
+ * @file 0_puts_recursion.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
