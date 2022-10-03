5e9d41794554d788e65b0c525160b1f2da9b2606
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-03 20:48:20 +0100

    :recycle: refactored the structure of the code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_arry - creates and initialize an array of char
+ * create_array - creates and initialize an array of char
  * @size: (int) the size of the array
  * @c: (char) the character to initialize the array with
  *
@@ -16,14 +16,23 @@ char *create_array(unsigned int size, char c)
 	if (size > 0)
 	{
 		char *char_array = malloc(size * sizeof(char));
-		unsigned int i;
 
-		for (i = 0; i < size; i++)
+		if (char_array)
 		{
-			*(char_array + i) = c;
+			unsigned int i;
+
+			for (i = 0; i < size; i++)
+			{
+				*(char_array + i) = c;
+			}
+			return (char_array);
 		}
-		return (char_array);
+		else
+		{
+			return (NULL);
+		}
 	}
+
 	else
 	{
 		return (NULL);
