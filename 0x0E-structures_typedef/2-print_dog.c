6349b0008cd294c659a66db99fc63187b6938739
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-10 18:45:23 +0100

    :ambulance: added error management

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,5 +12,6 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (d)
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
