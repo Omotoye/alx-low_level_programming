b2d429144a51203327f78a48fee6ccc304d41041
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-10 18:07:46 +0100

    :card_file_box: added header file for dog struct definition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog information
+ * @name: (char*) the name of the dog
+ * @age: (float) the age of the dog
+ * @owner: (char*) the name of the owner of the dog
+ *
+ * Description: a struct that stores the information of a dogi
+ * @author Omotoye Shamsudeen Adekoya
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
