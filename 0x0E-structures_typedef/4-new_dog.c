9c16879d6ddc004d1360d171bf22044ca5cb2a58
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-10 19:48:45 +0100

    :construction: <testing>

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,17 +15,44 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	char name_cpy[200];
+	char owner_cpy[200];
 
 	dog = malloc(sizeof(dog_t));
 	if (dog)
 	{
-		dog->name = name;
+		dog->name = _strcpy(name_cpy, name);
 		dog->age = age;
-		dog->owner = owner;
+		dog->owner = _strcpy(owner_cpy, owner);
 		return (dog);
 	}
 	else
 	{
 		return (NULL);
 	}
+}
+
+/**
+ * _strcpy - copies string from one buffer to another
+ * @dest: (char*) pointer to the destination buffer
+ * @src: (char*) pointer to the source buffer.
+ *
+ * @brief A function string from a source buffer to a destination
+ *     buffer and returns the pointer to the destination buffer
+ * Return: (char*) pointer to the destination buffer.
+ * @file 9-strcpy.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (1)
+	{
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
+			break;
+		i++;
+	}
+	return (dest);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -65,4 +65,17 @@ void print_dog(struct dog *d);
  */
 dog_t *new_dog(char *name, float age, char *owner);
 
+/**
+ * _strcpy - copies string from one buffer to another
+ * @dest: (char*) pointer to the destination buffer
+ * @src: (char*) pointer to the source buffer.
+ *
+ * @brief A function string from a source buffer to a destination
+ *     buffer and returns the pointer to the destination buffer
+ * Return: (char*) pointer to the destination buffer.
+ * @file 9-strcpy.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+char *_strcpy(char *dest, char *src);
+
 #endif /* DOG_H */
