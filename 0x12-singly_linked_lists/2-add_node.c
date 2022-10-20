ee1096d7a95d45528455f5c2478b3d3c8a94595c
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-20 16:05:19 +0100

    :adhesive_bandage: <minor change> handled a possible error

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,6 +17,9 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	unsigned int len = 0;
 
+	if (*head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
