19dc021923a39e4a39d78d25bb751157312ef88e
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-20 14:58:40 +0100

    :hammer: added a function to print all the elements of a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * print_list - print all elements of a list
+ * @h: (const list_t*) a pointer to the list to be printed
+ *
+ * @brief A function that prints all the elements of a singly
+ *     linked list
+ * Return: (size_t) the number of elements in the list
+ * @file 0-print_list.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *list;
+	size_t element_len = 0;
+
+	list = h;
+	while (1)
+	{
+		if (list->str)
+			printf("[%d] %s\n", list->len, list->str);
+		else
+			printf("[0] (nil)\n");
+		element_len++;
+		if (list->next)
+			list = list->next;
+		else
+			break;
+	}
+
+	return (element_len);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,45 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - puts character in stdout
+ * @c: (char) the character to be printed
+ *
+ * @brief A function to put a character into the standard
+ *     output file.
+ * Return: (void)
+ * @file _putchar.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void _putchar(char c);
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/**
+ * print_list - print all elements of a list
+ * @h: (const list_t*) a pointer to the list to be printed
+ *
+ * @brief A function that prints all the elements of a singly
+ *     linked list
+ * Return: (size_t) the number of elements in the list
+ * @file 0-print_list.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
