9d4a1ba8dfbe945bc5735bf615a92d3bdbaad1d6
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-21 02:22:28 +0100

    :construction: <testing> my c to assembly convertion

---
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @brief A program to print Hello, Holberton to the stdout
+ * Return: (void)
+ * @file 101-hello_holberton.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void main (void)
+{
+	printf("Hello, Holberton\n");
+}
