f09318aea73767467895b2d13b014460d8c68090
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-26 05:49:48 +0100

    :construction: <testing>

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,9 +17,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *next, *previous;
 	unsigned int node_num = 0;
 
-	if (*head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
