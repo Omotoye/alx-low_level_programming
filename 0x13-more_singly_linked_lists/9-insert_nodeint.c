37d08a10d39960c646c4eb6f661475e837fda27d
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-26 05:46:42 +0100

    :construction: <testing>

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,8 +26,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = (*head)->next;
+		new->next = (*head);
 		*head = new;
+		return (new);
 	}
 	while (next)
 	{
