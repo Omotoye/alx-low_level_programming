7521ed67582cf1b3da0072b1af1c033971336361
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-28 01:26:41 +0100

    :hammer: added function to set the bit in a number

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * set_bit - set bit to 1 at a given index
+ * @n: (unsigned long int) the number to set bit from
+ * @index: (unsigned int) the index to set the bit at
+ *
+ * @brief A function that set the value of a bit to `1` at 
+ *     a given index
+ * Return: (int) 1 if successful -1 if not successful 
+ * @file 3-set_bit.c 
+ * @author Omotoye Shamsudeen Adekoya
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit_mask = 1;
+
+	while (n != NULL && index < 64)
+	{
+		if (!index)
+		{
+			*n |= bit_mask;
+			return (1);
+		}
+		index--;
+		bit_mask <<= 1;
+	}
+	return (-1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -42,4 +42,17 @@ void print_binary(unsigned long int n);
  */
 int get_bit(unsigned long int n, unsigned int index);
 
+/**
+ * set_bit - set bit to 1 at a given index
+ * @n: (unsigned long int) the number to set bit from
+ * @index: (unsigned int) the index to set the bit at
+ *
+ * @brief A function that set the value of a bit to `1` at 
+ *     a given index
+ * Return: (int) 1 if successful -1 if not successful 
+ * @file 3-set_bit.c 
+ * @author Omotoye Shamsudeen Adekoya
+ */
+int set_bit(unsigned long int *n, unsigned int index);
+
 #endif /* MAIN_H*/
