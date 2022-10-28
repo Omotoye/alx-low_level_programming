6dcf95c4bd770e8985b66c781d56a719d79ed7c4
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2022-10-28 01:32:52 +0100

    :art: fixed the code forrmating

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,10 +5,10 @@
  * @n: (unsigned long int) the number to set bit from
  * @index: (unsigned int) the index to set the bit at
  *
- * @brief A function that set the value of a bit to `1` at 
+ * @brief A function that set the value of a bit to `1` at
  *     a given index
- * Return: (int) 1 if successful -1 if not successful 
- * @file 3-set_bit.c 
+ * Return: (int) 1 if successful -1 if not successful
+ * @file 3-set_bit.c
  * @author Omotoye Shamsudeen Adekoya
  */
 int set_bit(unsigned long int *n, unsigned int index)
