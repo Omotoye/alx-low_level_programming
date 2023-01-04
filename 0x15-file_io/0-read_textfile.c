b03d78e8ac240fa29851922bb9005a06769fe5f0
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2023-01-04 23:31:10 +0100

    :construction: testing...

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary of a number
+ * @n: (unsigned long int) the decimal number to be converted to
+ *     bianry
+ *
+ * @brief A function that takes in a decimal number and prints
+ *     the binary representation of that number
+ * Return: (void)
+ * @file 1-print_binary.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * _putchar - puts character in stdout
+ * @c: (char) the character to be printed
+ *
+ * @brief A function to put a character into the standard
+ *     output file.
+ * Return: (void)
+ * @file _putchar.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+void _putchar(char c);
+
+#endif /* MAIN_H*/
