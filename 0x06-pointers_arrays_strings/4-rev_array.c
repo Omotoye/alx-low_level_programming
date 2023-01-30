2c1350c7b08a91a5a96012a285ee737a19e10347
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2023-01-30 17:17:20 +0100

    :hammer: added a function that reverses the content of an array of integer

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array
+ * @a: (int*) array of integer
+ * @n: (int) the number of elements in the array
+ *
+ * @brief A function that reverses the content of an array of integers
+ * Return: (void)
+ * @file 4-rev_array.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+
+	for (int i = 0; i <= (n - 1) / 2; i++)
+	{
+		temp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = temp;
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -58,4 +58,17 @@ char *_strncpy(char *dest, char *src, int n);
 
 int _strcmp(char *s1, char *s2);
 
+/**
+ * reverse_array - reverses the content of an array
+ * @a: (int*) array of integer
+ * @n: (int) the number of elements in the array
+ *
+ * @brief A function that reverses the content of an array of integers
+ * Return: (void)
+ * @file 4-rev_array.c
+ * @author Omotoye Shamsudeen Adekoya
+ */
+
+void reverse_array(int *a, int n);
+
 #endif /* MAIN_H */
